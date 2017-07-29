1fe6d1895bbe57db088d4620cc68be6e0f6911eb
Philippe Gibert <philippe.gibert@gmail.com>
2017-07-29 14:18:39 +0200

    Use env vart to server port, default 3000

--- server.js
+++ server.js
@@ -6,11 +6,12 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
 const handler = routes.getRequestHandler(app)
 
+const PORT = process.env.PORT ? process.env.PORT : 3000
 app.prepare()
-    .then(() => {
-        createServer(handler)
-            .listen(3000, (err) => {
-                if (err) throw err
-                console.log('> Ready on http://localhost:3000')
-            })
-    })
+  .then(() => {
+    createServer(handler)
+      .listen(3000, (err) => {
+        if (err) throw err
+        console.log('> Ready on http://localhost:' + PORT)
+      })
+  })
