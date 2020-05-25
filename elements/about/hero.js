29c6d20c8c613ad2c1a7c25f25d3ee6e8022e5d5
Philippe Gibert <philippe.gibert@gmail.com>
2020-05-26 00:04:05 +0200

    Fix profil

--- elements/about/hero.js
+++ elements/about/hero.js
@@ -5,7 +5,7 @@ export default () => (
         <div className="hero-body">
             <div className="container">
                 <h1 className="title is-1">Philippe Gibert</h1>
-                <div className="subtitle is-3">Développeur JS & PHP</div>
+                <div className="subtitle is-3">Développeur FullStack</div>
             </div>
         </div>
     </section>
--- elements/about/profil.js
+++ elements/about/profil.js
@@ -3,44 +3,52 @@ import moment from 'moment'
 
 import Marker from './utils/marker'
 
-export default () => (
+export default () => {
+  const beginWork = moment(['2003', '07', '01']);
+  const nbYearsOfWork = moment().diff(beginWork, 'years');
+
+  return (
     <section className="section">
-        <div className="container">
-            <div className="columns">
-                <div className="column is-offset-1 is-10 has-text-centered">
-                    <h2 className="title is-2">Profil</h2>
-                    <hr/>
-                </div>
-            </div>
-            <div className="columns">
-                <div className="column is-3 is-offset-1 has-text-left">
-                    <h3 className="title is-3">A propos de moi </h3>
-                    <div className="content">
-                        <p>
-                            Développeur Web depuis plus de 10 ans, j'ai une bonne connaissance des techniques back-end et front-end.
-                            J'aime faire une veille constante sur l'ecosysteme web et découvrir de nouvelles technologies.
-                            Passionné d'internet, de jeux de sociétés et de tout ce qui tourne autour de l'univers geek et high tech, je vous fais partager mes passions
-                            à travers mon blog.
-                        </p>
-                    </div>
-                </div>
-                <div className="column is-2 is-offset-1 has-text-centered">
-                    <figure className="image is-1by1" >
-                        <img src="/static/images/giboow-portrait.jpg" style={{borderRadius:"1000px"}}/>
-                    </figure>
-                </div>
-                <div className="column is-3 is-offset-1 has-text-left">
-                    <h3 className="title is-3">Details</h3>
-                    <p>
-                        <strong>Nom :</strong><br/>
-                        Philippe Gibert<br/>
-                        <strong>Age :</strong><br/>
-                        {moment().diff('1984-12-15', 'years')} ans<br/>
-                        <strong>Situation géographique :</strong><br/>
-                        <Marker city="Bain de Bretagne, Ille-et-Vilaine, France"/>
-                    </p>
-                </div>
-            </div>
+      <div className="container">
+        <div className="columns">
+          <div className="column is-offset-1 is-10 has-text-centered">
+            <h2 className="title is-2">Profil</h2>
+            <hr/>
+          </div>
         </div>
+        <div className="columns">
+          <div className="column is-3 is-offset-1 has-text-left">
+            <h3 className="title is-3">A propos de moi </h3>
+            <div className="content">
+              <p>
+                Développeur Web depuis plus de {nbYearsOfWork} ans, j'ai une bonne connaissance
+                des techniques back-end et front-end.
+                J'aime faire une veille constante sur l'ecosysteme web et découvrir de nouvelles
+                technologies.
+                Passionné d'internet, de jeux de sociétés et de tout ce qui tourne autour de
+                l'univers geek et high tech, je vous fais partager mes passions
+                à travers mon blog.
+              </p>
+            </div>
+          </div>
+          <div className="column is-2 is-offset-1 has-text-centered">
+            <figure className="image is-1by1">
+              <img src="/static/images/giboow-portrait.jpg" style={{borderRadius: "1000px"}}/>
+            </figure>
+          </div>
+          <div className="column is-3 is-offset-1 has-text-left">
+            <h3 className="title is-3">Details</h3>
+            <p>
+              <strong>Nom :</strong><br/>
+              Philippe Gibert<br/>
+              <strong>Age :</strong><br/>
+              {moment().diff('1984-12-15', 'years')} ans<br/>
+              <strong>Situation géographique :</strong><br/>
+              <Marker city="Bain de Bretagne, Ille-et-Vilaine, France"/>
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
-)
+  )
+}
