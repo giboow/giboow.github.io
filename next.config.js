d2fdf7013d18e0a6aa97b1572e1ff19299e2e991
Philippe Gibert <philippe.gibert@gmail.com>
2020-06-07 23:38:32 +0200

    Add Fontawesome react lib

--- next.config.js
+++ next.config.js
@@ -1,22 +1,26 @@
 const withSass = require('@zeit/next-sass')
+const withCSS = require('@zeit/next-css')
 
-module.exports = withSass({
-  distDir: "_next",
-  generateBuildId: async () => {
-    if (process.env.BUILD_ID) {
-      return process.env.BUILD_ID;
-    } else {
-      return `${new Date().getTime()}`;
-    }
-  },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: 'empty'
+
+module.exports = withCSS(
+  withSass({
+    distDir: "_next",
+    generateBuildId: async () => {
+      if (process.env.BUILD_ID) {
+        return process.env.BUILD_ID;
+      } else {
+        return `${new Date().getTime()}`;
       }
-    }
+    },
+    webpack: (config, {isServer}) => {
+      // Fixes npm packages that depend on `fs` module
+      if (!isServer) {
+        config.node = {
+          fs: 'empty'
+        }
+      }
 
-    return config
-  }
-});
+      return config
+    }
+  })
+);
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import App from 'next/app'
 import { ThemeProvider } from 'styled-components'
-
 import style from '../styles/index.scss'
+
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css' // Import the CSS
+config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
 
 export default class MyApp extends App {
   render() {
