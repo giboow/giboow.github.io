37e7a89aa9f7c360abcb483e4ffabc3bd4b0c2a3
Philippe Gibert <philippe.gibert@gmail.com>
2021-10-14 16:30:34 +0200

    fix change nextjs version

--- next.config.js
+++ next.config.js
@@ -1,26 +1,16 @@
 const path = require('path')
 
-
 module.exports = {
-    distDir: "_next",
+    distDir: 'build',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
     generateBuildId: async () => {
-      if (process.env.BUILD_ID) {
-        return process.env.BUILD_ID;
-      } else {
-        return `${new Date().getTime()}`;
-      }
-    },
-    webpack: (config, {isServer}) => {
-      // Fixes npm packages that depend on `fs` module
-      if (!isServer) {
-        config.node = {
-          fs: 'empty'
+        if (process.env.BUILD_ID) {
+            return process.env.BUILD_ID;
+        } else {
+            return `${new Date().getTime()}`;
         }
-      }
+    },
+}
 
-      return config
-    }
-  };
